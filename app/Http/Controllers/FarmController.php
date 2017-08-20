bc2b25374cc50925694bcf10a84e5fea59e7d045
Markuz <krupa_marek@yahoo.com>
2017-08-20 12:21:27 +0200

    Add controllers Farm/Market

---
+++ app/Http/Controllers/FarmController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Farm;
+use Illuminate\Http\Request;
+
+class FarmController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $farm = Farm::orderBy('name', 'asc')->paginate(9);
+        return view('farms.index', ['farm' => $farm]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('farms.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Farm::create($request->all());
+        return redirect('farms')->wiht('status', 'New Farm was created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Farm  $farm
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Farm $farm)
+    {
+        return wiew('farms.show', compact('farm'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Farm  $farm
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Farm $farm)
+    {
+        return view('farms.edit', ['farm' => $farm]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Farm  $farm
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Farm $farm)
+    {
+        $farm->update($request->all());
+        return redirect('farms');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Farm  $farm
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Farm $farm)
+    {
+        $farm->delete();
+        return redirect('farms');
+    }
+}
